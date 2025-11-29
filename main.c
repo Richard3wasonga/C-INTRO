1cf3729fcf123d4d607ee2c17c557602918fcd83
Richard Wasonga <rowwasonga@gmail.com>
2025-11-29 11:35:49 +0300

    update in size of and characters

--- main.c
+++ main.c
@@ -5,14 +5,26 @@ int main(){
     int age = 10;
     printf("Age = %d\n", age);
     */
-   /*
-   double number = 12.45;
-   float number1 = 10.9f;
-   double number2 = 5.5e6;
-   printf("%.2lf\n", number);
-   printf("%.1f\n", number1);
-   printf("%lf\n", number2);
-   */
+    /*
+    double number = 12.45;
+    float number1 = 10.9f;
+    double number2 = 5.5e6;
+    printf("%.2lf\n", number);
+    printf("%.1f\n", number1);
+    printf("%lf\n", number2);
+    */
+    /*
+    char character = 'z';
+    printf("%c\n", character);
+    printf("%d\n", character);
+    */
+    /*
+    int age;
+    double number;
+    printf("int size = %zu\n", sizeof (age));
+    printf("double size = %zu\n", sizeof (number));
+    */
+
 
     return 0;
     
