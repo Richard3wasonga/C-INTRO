bc69536001f115af420bb3e3889d3f6ef1c93f11
Richard Wasonga <rowwasonga@gmail.com>
2025-11-29 11:22:41 +0300

    update on double and float data type

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
 int main(){
+    /*
     int age = 10;
     printf("Age = %d\n", age);
+    */
+   /*
+   double number = 12.45;
+   float number1 = 10.9f;
+   double number2 = 5.5e6;
+   printf("%.2lf\n", number);
+   printf("%.1f\n", number1);
+   printf("%lf\n", number2);
+   */
+
     return 0;
+    
 }
