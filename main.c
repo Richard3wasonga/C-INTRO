4de4af451194cd900cf8abbc021d2fac370ffd6f
Richard Wasonga <rowwasonga@gmail.com>
2025-11-30 21:53:41 +0300

    Update on c loops and conditional statements

--- main.c
+++ main.c
@@ -85,6 +85,259 @@ int main(){
     */
 
     /*Do while loop*/
+    /*
+    int count = 1;
+    do{
+        printf("%d\n", count);
+        count = count + 1;
+    }while (count < 5);
+    */
+
+    /*The diffrence between the do while and the while loop is that do while loop is run once no matter what*/
+    /*
+    int number;
+    int count = 10;
+
+    printf("Enter the number: ");
+    scanf("%d", &number);
+
+    while(count != 0){
+        int product = number * count;
+        printf("%d*%d = %d\n", number, count, product);
+        count = count - 1;
+    }
+
+    */
+
+    /*For loop*/
+
+    /*
+    int sum = 0;
+    for(int i = 2; i <= 100; i+2){
+        sum = sum + i;
+    }
+    printf("%d\n", sum);
+
+    */
+
+    /*Break and Continue*/
+    
+    /*
+    for(int i = 1; i <= 5; i++){
+        if (i == 3){
+            break;
+        }
+        printf("%d\n", i);
+        
+    }
+
+    */
+    /*
+    while(1){
+        int number;
+        printf("Enter a number: ");
+        scanf("%d", &number);
+        
+        if(number < 0){
+            break;
+        }
+    }
+
+    */
+
+    /*
+
+    for(int i = 1; i <= 5; i++){
+        if (i == 3){
+            continue;
+        }
+        printf("%d\n", i);
+        
+    }
+
+    */
+
+    /*break statement stops the loop completly while in continue it skips the current iteration*/
+
+    /*
+    while(1){
+        int number;
+        printf("Enter a number: ");
+        scanf("%d", &number);
+
+        if (number <= 0){
+            break;
+        }
+        if((number % 2) != 0){
+            continue;
+        }
+        printf("%d\n", number);
+    }
+
+    */
+
+    /*
+
+    while(1){
+        int number;
+        printf("Enter a number: ");
+        scanf("%d", &number);
+
+        if (number >= 0){
+            printf("Positive Value\n");
+            break;
+        }
+        if(number % 2 == 0){
+            printf("Negative Even\n");
+            continue;
+        }
+        printf("%d\n", number);
+    }
+
+    */
+    
+    
+    /*If else statement*/
+    /*
+    int age;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    if(age > 120 || age < 0){
+        printf("Invalid age\n");
+    }
+    else if(age >= 18){
+        printf("You are eligible to vote\n");
+    }
+    else{
+        printf("Sorry you are not eligible to vote\n");
+    }
+
+    */
+
+    /*Ternary operator*/
+
+    /*
+
+    char operator = '+';
+
+    int num1 = 8;
+    int num2 = 7;
+
+
+    int result = (operator == '+') ? (num1 + num2) : (num1 - num2);
+    printf("%d\n", result);
+
+    */
+
+    /*switch statement*/
+
+    /*Printing one satatement for every options*/
+
+    /*
+    int number;
+    printf("Enter a number between 1 to 7: ");
+    scanf("%d", &number);
+
+    switch(number){
+        case 1:
+        printf("sunday\n");
+        break;
+
+        case 2:
+        printf("Monday\n");
+        break;
+
+        case 3:
+        printf("Tuesday\n");
+        break;
+
+        case 4:
+        printf("wednesday\n");
+        break;
+
+        case 5:
+        printf("Thursday\n");
+        break;
+
+        case 6:
+        printf("Friday\n");
+        break;
+
+        case 7:
+        printf("Saterday\n");
+        break;
+
+        default:
+        printf("Invalid Number\n");
+    }
+
+    */
+
+    /*Printing one satatement for multiple options*/
+
+    /*
+    int number;
+    printf("Enter a number between 1 to 7: ");
+    scanf("%d", &number);
+
+    switch(number){
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        printf("Weekday\n");
+        break;
+
+        case 1:
+        case 7:
+        printf("Weekend\n");
+        break;
+
+        default:
+        printf("Invalid Number\n");
+    }
+
+    */
+
+    /*
+
+    char operator;
+    printf("Choose an operator ['+', '-', '*', '/']: ");
+    scanf("%c", &operator);
+  
+    double num1, num2;
+  
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+  
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+  
+    double result;
+
+    switch(operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '/':
+        result = num1 / num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+
+      default:
+        printf("Invalid Operator");
+    }
+  
+    printf("%.2lf\n", result);
+
+    */
 
     return 0;
     
