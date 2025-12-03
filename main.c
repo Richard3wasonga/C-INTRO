5d01a17816ad22693792a10769f259306f3cf374
Richard Wasonga <rowwasonga@gmail.com>
2025-12-03 21:24:48 +0300

    bitwise operations

--- main.c
+++ main.c
@@ -418,6 +418,29 @@ int main(){
     printf("%d\n", result);
     */
 
+    /*Bitwise operator*/
+
+    /*
+
+    int a = 4, b = 8;
+    int c;
+
+    c = a & b;
+    printf("a & b : %d\n", c);
+
+    c = a | b;
+    printf("a | b : %d\n", c);
+
+    c = a << 2;
+    printf("a << 2 : %d\n", c);
+
+    c = a >> 2;
+    printf("a >> 2 : %d\n", c);
+
+    c = a ^ b;
+    printf("a ^ b : %d\n", c);
+    */
+
     return 0;
     
 }
